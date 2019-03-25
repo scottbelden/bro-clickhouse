702362722ba0bfbdf0f7c2f7d92aa6e65ecf1f8b
scottbelden <scottabelden@gmail.com>
2019-03-25 16:49:46 -0400

    add radius schema

--- schemas/reporter.sql
+++ schemas/reporter.sql
@@ -3,6 +3,6 @@ day Date DEFAULT toDate(ts),
 ts DateTime,
 level String,
 message String,
-location String
+location Nullable(String)
 )
 ENGINE = MergeTree(day,sipHash64(message), (day,sipHash64(message), message), 8192);
