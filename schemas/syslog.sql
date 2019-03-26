b2368c219b8cfb40e6482e64a63f1870f88594b1
scottbelden <scottabelden@gmail.com>
2019-03-26 17:46:42 -0400

    add entry for strange syslog facility value

--- schemas/syslog.sql
+++ schemas/syslog.sql
@@ -8,7 +8,7 @@ create table syslog
 	resp_h String,
 	resp_p UInt16,
 	proto Enum8('tcp' = 1, 'udp' = 2),
-	facility Enum8('ALERT' = 1, 'AUTH' = 2, 'AUTHPRIV' = 3, 'CRON' = 4, 'DAEMON' = 5, 'KERN' = 6, 'LOCAL0' = 7, 'LOCAL1' = 8, 'LOCAL2' = 9, 'LOCAL3' = 10, 'LOCAL4' = 11, 'LOCAL5' = 12, 'LOCAL6' = 13, 'LOCAL7' = 14, 'MAIL' = 15, 'NTP' = 16, 'SYSLOG' = 17, 'USER' = 18),
+	facility Enum8('ALERT' = 1, 'AUTH' = 2, 'AUTHPRIV' = 3, 'CRON' = 4, 'DAEMON' = 5, 'KERN' = 6, 'LOCAL0' = 7, 'LOCAL1' = 8, 'LOCAL2' = 9, 'LOCAL3' = 10, 'LOCAL4' = 11, 'LOCAL5' = 12, 'LOCAL6' = 13, 'LOCAL7' = 14, 'MAIL' = 15, 'NTP' = 16, 'SYSLOG' = 17, 'USER' = 18, '?-127' = 127),
 	severity Enum8('ALERT' = 1, 'CRIT' = 2, 'DEBUG' = 3, 'ERR' = 4, 'INFO' = 5, 'NOTICE' = 6, 'WARNING' = 7, 'EMERG' = 8),
 	message String,
 	schema UInt32
